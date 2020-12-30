7cf2060585402485a9158ee527874a3bc3bd3e07
Jorik <jorik.slotman@gmail.com>
2020-12-30 22:23:59 +0100

    back-end changes survey

--- cattleweb_root/cattleweb/apps/survey/admin.py
+++ cattleweb_root/cattleweb/apps/survey/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Survey,Question,Answer,VisitEvent
+from .models import Survey,Question,Answer,Response
 
 # Register your models here.
-admin.site.register([Survey,Question,VisitEvent])
+admin.site.register([Survey,Question,Answer,Response])
