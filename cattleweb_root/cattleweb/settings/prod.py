2e83dce0343075c20cf1c7bf3ea3bdbcfb2cf713
Jorik <jorik.slotman@gmail.com>
2021-01-19 21:43:24 +0100

    prod allowed host all - roll back!

--- cattleweb_root/cattleweb/settings/prod.py
+++ cattleweb_root/cattleweb/settings/prod.py
@@ -4,7 +4,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
